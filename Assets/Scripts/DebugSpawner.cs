bfe0f802fd5158025f83c7f134181a3abd76296a
Uzabi <107811072+veyetazo@users.noreply.github.com>
2023-10-13 13:02:12 +0200

    initial spawn logic

--- Assets/Scripts/DebugSpawner.cs
+++ Assets/Scripts/DebugSpawner.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class DebugSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Sapwn logic
     }
 }
