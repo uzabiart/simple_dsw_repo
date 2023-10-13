81d9758fbe3afccc2f079ce24561f3d7eddac756
Uzabi <107811072+veyetazo@users.noreply.github.com>
2023-10-13 13:14:58 +0200

    car changes

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("001");
     }
 }
